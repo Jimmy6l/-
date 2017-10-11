5299e6e7756ee14d4b2ef9df54a3b456e14535b6
Jimmy6l <31723994+Jimmy6l@users.noreply.github.com>
2017-10-11 20:30:04 +0800

    Update 4_2.cpp

--- 4_2.cpp
+++ 4_2.cpp
@@ -3,6 +3,7 @@
 void main()
 {
 int a,leap,i=0;
+cout<<"2000年到3000年所有闰年年份:"<<endl;
 for(a=2000;a<=3000;a++)
 {
 	leap=1;
@@ -14,9 +15,11 @@ for(a=2000;a<=3000;a++)
 	}
 	if(leap)
 	{  
-		cout<<" "<<a;
+		cout<<"  "<<a;
 		i++;
 		if(i%10==0)
 		cout<<endl;
-	}	
-}}
+}}	
+	if(i%10!=0)
+	cout<<"  i="<<i<<endl;
+}
