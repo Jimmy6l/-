5668308443c039a0faa11baf6c4a4a7ba064c031
Jimmy6l <31723994+Jimmy6l@users.noreply.github.com>
2017-11-07 17:30:12 +0800

    Add files via upload

---
+++ ex6_9.cpp
@@ -0,0 +1,22 @@
+#include <iostream.h>
+void fun(int,int)
+void main()
+{
+	int m,n,k;
+	cin>>m>>n;
+	k=0;
+	k++;
+	fun(m,n);
+	cout<<m<<n<<endl;
+}
+void fun(int i;int j)
+{int t
+	for(t=1;t<=m;t++)
+	{
+		if(k%7==0&&k%11==0)
+			n++;
+	}
+}
+
+
+
