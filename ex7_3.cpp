9cfaf59291204995727d2f47ea14cb50d8fc4876
Jimmy6l <31723994+Jimmy6l@users.noreply.github.com>
2017-12-21 16:55:40 +0800

    Add files via upload

---
+++ ex7_3.cpp
@@ -0,0 +1,37 @@
+#include<iostream.h>
+void main()
+{
+	int a[10],l,max,min,m,n,i,j;
+    cout<<"调换位置后的数组: "<<endl;
+	for(i=0;i<10;i++)
+		cin>>a[i];
+	int*p,*q;
+	p=q=a;
+	max=min=a[0];
+	for(i=1;i<10;i++)
+	{
+		if(*(p+i)>max)
+		{
+			max=*(p+i);
+			m=i;
+		}
+
+	}
+	for(j=1;j<10;j++)
+	{
+		if(*(q+j)<min)
+		{
+			min=*(q+j);
+			n=j;
+		}
+	}
+	int t;
+	t=*(p+m);
+	*(p+m)=*(q+n);
+	*(q+n)=t;
+	for(i=0;i<10;i++)
+	{
+		cout<<a[i]<<"  ";
+	}
+
+}
