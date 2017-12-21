eac14c95e3aff553e99636c8e7f7467068055911
Jimmy6l <31723994+Jimmy6l@users.noreply.github.com>
2017-12-21 16:56:36 +0800

    Add files via upload

---
+++ zy7_4.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+void main()
+{
+	int a[10],i,j;
+    cout<<"·´ÐòÊý×é: "<<endl;
+	for(i=0;i<10;i++)
+		cin>>a[i];
+	int*p,*q;
+	p=a;
+	q=&a[9];
+	int t;
+	while(p<q)
+	{
+	    t=*p;
+	    *p=*q;
+	    *q=t;
+		p++;
+		q--;
+	}
+    for(i=0;i<10;i++)
+	cout<<a[i]<<" ";
+	cout<<endl;
+}
